10a7ecf5c957b32a07e597fd84b5f91fd3aaa759
whitevamp <whitevamp@duck.com>
2024-09-20 23:24:32 -0700

    removed extra entries in SkyRim_Start_Bat script.

--- SkyRim_Start_Bat/start.bat
+++ SkyRim_Start_Bat/start.bat
@@ -2,13 +2,11 @@
 setlocal enabledelayedexpansion
 
 REM Set the source and destination directories
-:: Set the source and destination directories
-set "source_dir=I:\Games\Novuls\Instances\Nolvus Ascension\MODS\mods\MISC_Anime_Main_Menu_Replace_Collection\Pic"
-REM set "dest_dir=I:\Games\Novuls\Instances\Nolvus Ascension\MODS\mods\MISC_Anime_Main_Menu_Replace_Collection\textures\interface\objects"
-set "dest_dir=I:\Games\Novuls\Instances\Nolvus Ascension\MODS\mods\SOULWORKER MAINMENU\textures\interface\objects"
+set "source_dir=C:\path\to\source\folder"
+set "dest_dir=C:\path\to\destination\folder"
 
 REM Set the new name for the copied file (without the path)
-set "new_file_name=MAINMENUWALLPAPER.dds"
+set "new_file_name=newname.dds"
 
 REM Check if the source directory exists
 if not exist "%source_dir%" (
@@ -43,17 +41,15 @@ REM Randomly select a file from the source directory
 set /a random_index=%random%%%file_count+1
 set "random_file=!file_%random_index%!"
 
-REM Copy the randomly selected file to the destination directory and rename it
-echo Copying file: !random_file!
+REM Copy and rename the randomly selected file to the destination directory
+echo Copying file: !random_file! and renaming to: %new_file_name%
 copy /y "!random_file!" "%dest_dir%\%new_file_name%"
 
 REM Wait for 5 seconds before launching the program to ensure copying is complete
 timeout /t 5 /nobreak >nul
 
 REM Launch the program (e.g., SKSE or any executable)
-echo Starting the next program...
-start "" "I:\Games\Novuls\Instances\Nolvus Ascension\STOCK GAME\skse64_loader.exe"
+start "" "C:\path\to\your\program.exe"
 
 echo Done!
 pause
-
